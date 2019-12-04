bfe66b94c1aa8369b79254de3c321050141e7ac0
Mathias <mlj.bouillon@outlook.fr>
2019-12-04 15:40:35 +0100

    save my work

---
+++ src/Controller/GabareLifeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GabareLifeController extends AbstractController
+{
+    /**
+     * @Route("/vie-cooperative", name="gabare_life")
+     */
+    public function index()
+    {
+        return $this->render('gabare_life/index.html.twig');
+    }
+}
