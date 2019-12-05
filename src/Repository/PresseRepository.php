4e5adf70abd4b18dee2062e120c5b05b648c95f2
Mathias <mlj.bouillon@outlook.fr>
2019-12-05 14:17:44 +0100

    delete some comment and migration

--- src/Repository/PresseRepository.php
+++ src/Repository/PresseRepository.php
@@ -18,33 +18,4 @@ class PresseRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Presse::class);
     }
-
-    // /**
-    //  * @return Presse[] Returns an array of Presse objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Presse
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
