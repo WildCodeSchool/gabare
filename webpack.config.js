64f907096cb13cce269547d9905b5c11d4439659
wilder <anthonykwanchung@gmail.com>
2019-12-09 17:59:52 +0100

    css corrections

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-// directory where compiled assets will be stored
+    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
 
     .enableVersioning()
@@ -12,6 +12,15 @@ Encore
     //.setManifestKeyPrefix('build/')
     .copyFiles({
         from: './assets/images',
+
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
     })
 
     /*
