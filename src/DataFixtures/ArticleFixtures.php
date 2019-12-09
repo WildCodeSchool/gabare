a8b92430e27163e7d9583be48bba0de70b2d9801
wilder <anthonykwanchung@gmail.com>
2019-12-09 23:34:38 +0100

    travis correction

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -18,7 +18,7 @@ class ArticleFixtures extends Fixture implements DependentFixtureInterface
             $actualities = new Article();
             $actualities->setImage($faker->imageUrl())
                 ->setTitle($faker->sentence())
-                ->setDescription($faker->text($maxNbChars = 400))
+                ->setDescription($faker->text($max = 400))
                 ->setDate($faker->DateTime());
             $actualities->setTheme($this->getReference('themes_'.random_int(0, count(ThemeFixtures::THEMES)-1)));
             $manager->persist($actualities);
