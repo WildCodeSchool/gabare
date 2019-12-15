764a6500f42b6db253d2fa6903a0904c39e45681
Lionel Delamare <lionel.delamare@hotmail.com>
2019-12-15 16:38:16 +0100

    Adding CKeditor on webpack_config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -86,4 +86,13 @@ Encore
     { from: './assets/images', to: 'images' }
 ]));
 
+.copyFiles([
+    {from: './node_modules/ckeditor/', to: 'ckeditor/[path][name].[ext]', pattern: /\.(js|css)$/, includeSubdirectories: false},
+    {from: './node_modules/ckeditor/adapters', to: 'ckeditor/adapters/[path][name].[ext]'},
+    {from: './node_modules/ckeditor/lang', to: 'ckeditor/lang/[path][name].[ext]'},
+    {from: './node_modules/ckeditor/plugins', to: 'ckeditor/plugins/[path][name].[ext]'},
+    {from: './node_modules/ckeditor/skins', to: 'ckeditor/skins/[path][name].[ext]'}
+])
+;
+
 module.exports = Encore.getWebpackConfig();
