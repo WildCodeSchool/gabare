91907fc086f53702bc3c1394986df07e7d80a4e5
wilder <anthonykwanchung@gmail.com>
2019-12-24 13:37:49 +0100

    Travis

--- src/Controller/AboutUsController.php
+++ src/Controller/AboutUsController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Contact;
 use App\Entity\Worker;
-use App\Form\ContactType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
