1c4794106f05a57c3f374f925a698403eb52fee3
Lionel Delamare <lionel.delamare@hotmail.com>
2020-01-06 13:27:46 +0100

    Adding asserts in History Entity and adding constraints in Carousel Entity

--- src/Entity/History.php
+++ src/Entity/History.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\HistoryRepository")
@@ -18,11 +19,13 @@ class History
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
      */
     private $description;
 
