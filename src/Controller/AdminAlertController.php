42a201b45815dc19183304400df5e73360cfc5c0
Barbara Carmé <barbara_carme@yahoo.fr>
2020-01-07 13:56:02 +0100

    fix sniffer

---
+++ src/Controller/AdminAlertController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Alert;
+use App\Form\AlertType;
+use App\Repository\AlertRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("admin/alerte")
+ */
+class AdminAlertController extends AbstractController
+{
+    /**
+     * @Route("/", name="alert_index", methods={"GET"})
+     */
+    public function index(AlertRepository $alertRepository): Response
+    {
+        return $this->render('admin_alert/index.html.twig', [
+            'alerts' => $alertRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}/edit", name="alert_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Alert $alert): Response
+    {
+        $form = $this->createForm(AlertType::class, $alert);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('alert_index');
+        }
+
+        return $this->render('admin_alert/edit.html.twig', [
+            'alert' => $alert,
+            'form' => $form->createView(),
+        ]);
+    }
+}
