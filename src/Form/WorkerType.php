9eb39fde00f4a6146d98bf3eef9e9b34c1c33e9c
wilder <anthonykwanchung@gmail.com>
2020-01-07 23:18:39 +0100

    Add Worker CRUD

--- src/Form/WorkerType.php
+++ src/Form/WorkerType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Activity;
 use App\Entity\Worker;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +16,24 @@ class WorkerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('function')
-            ->add('email')
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('function', TextType::class, [
+                'label' => 'Fonction',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
             ->add('portrait')
-            ->add('activity')
+            ->add('activity', EntityType::class, [
+                'class' => Activity::class,
+                'choice_label' => 'name',
+                'label' => 'Poste',
+            ])
         ;
     }
 
