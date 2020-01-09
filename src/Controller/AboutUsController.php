7ec91e5b631562eecaba9c598f7a9804bdc27dd0
wilder <anthonykwanchung@gmail.com>
2020-01-09 13:45:56 +0100

    correction done

--- src/Controller/AboutUsController.php
+++ src/Controller/AboutUsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Partner;
 use App\Entity\Worker;
 use App\Repository\WorkerRepository;
+use App\Repository\PartnerRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,14 +16,16 @@ class AboutUsController extends AbstractController
      * @Route("/qui-sommes-nous", name="about_us")
      * @return Response
      */
-    public function index(WorkerRepository $workerRepository): Response
+    public function index(WorkerRepository $workerRepository, PartnerRepository $partnerRepository): Response
     {
+        $partners = $partnerRepository->findAll();
 
         return $this->render('about_us/index.html.twig', [
             'pioneers' => $workerRepository->findAllPioneers(),
             'employees' => $workerRepository->findAllEmployees(),
             'CAMembers' => $workerRepository->findAllCAMembers(),
             'CAFriends' => $workerRepository->findAllCAFriends(),
+            'partners' => $partners,
         ]);
     }
 }
