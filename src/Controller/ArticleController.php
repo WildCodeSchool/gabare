335fdff479cc03418b148802533fb77c274b95f2
Lionel Delamare <lionel.delamare@hotmail.com>
2020-01-10 09:11:05 +0100

    Progression pagination

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
  * @Route("/article", name="article_")
@@ -13,21 +15,27 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends AbstractController
 {
+    const ARTICLES = 9;
+
     /**
-     * @Route("/", name="list")
+     * @Route("/{page}", name="list", requirements={"page" = "\d+"}, methods={"GET"}, defaults={"page" = 1})
      * @return Response
      */
-    public function list(): Response
+    public function list(ArticleRepository $articleRepository, int $page): Response
     {
         $articles = $this->getDoctrine()
             ->getRepository(Article::class)
             ->findBy(
                 [],
                 ['date' => 'DESC'],
-                20
+                self::ARTICLES
             );
+        $articles = $articleRepository->findAllPagesAndSort($page);
+        $nbArticles = count($articleRepository->findAllPagesAndSort());
         return $this->render('article/list.html.twig', [
             'articles' => $articles,
+            'page' => $page,
+            'nbPages' => ceil($nbArticles / self::ARTICLES),
         ]);
     }
 
--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -2,12 +2,10 @@
 
 namespace App\Repository;
 
+use App\Controller\ArticleController;
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
-use Doctrine\ORM\Tools\Pagination\Paginator;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use InvalidArgumentException;
 
 /**
  * @method Article|null find($id, $lockMode = null, $lockVersion = null)
@@ -22,34 +20,22 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
-    public function findAllPagesAndSort($page, $nbMaxByPage)
+    /**
+     * @param int $page
+     * @return array
+     */
+
+    public function findAllPagesAndSort($page = null)
     {
-        if (!is_numeric($page)) {
-            throw new InvalidArgumentException(
-                'La valeur de l\'argument $page est incorrecte (valeur : ' . $page .').'
-            );
-        }
-        if ($page < 1) {
-            throw new NotFoundHttpException('La page demandée n\'existe pas');
-        }
-        if (!is_numeric($nbMaxByPage)) {
-            throw new InvalidArgumentException(
-                'La valeur de l\'argument $nbMaxByPage est incorrecte (valeur : ' . $nbMaxByPage . ').'
-            );
-        }
         $qb = $this->createQueryBuilder('a')
-            ->where('CURRENT_DATE() >= a.datePublication')
-            ->orderBy('a.datePublication', 'DESC');
+            ->where('CURRENT_DATE() >= a.date')
+            ->orderBy('a.date', 'DESC');
 
-        $query = $qb->getQuery();
-
-        $firstResult = ($page - 1) * $nbMaxByPage;
-        $query->setFirstResult($firstResult)->setMaxResults($nbMaxByPage);
-        $paginator = new Paginator($query);
-
-        if (($paginator->count() <= $firstResult) && $page !=1) {
-            throw new NotFoundHttpException('La page demandée n\'existe pas.');
+        if ($page !== null) {
+            $firstResult = ($page - 1) * ArticleController::ARTICLES;
+            $qb->setFirstResult($firstResult)->setMaxResults(ArticleController::ARTICLES);
         }
-        return $paginator;
+
+        return $qb->getQuery()->getResult();
     }
 }
