fb3f289229ac7de41965df5df7229f6c9813b07c
wilder <anthonykwanchung@gmail.com>
2020-01-13 18:02:42 +0100

    crash request

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\SearchProductType;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use App\Service\ConnectOdooService;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,11 +16,18 @@ class ProductController extends AbstractController
 {
     /**
      * @Route("/nos-produits", name="products")
+     * @param ProductRepository $productRepository
+     * @param Request $request
+     * @param CategoryRepository $categoryRepository
      * @return Response
      */
 
-    public function index(ProductRepository $productRepository, Request $request): Response
-    {
+    public function index(
+        ProductRepository $productRepository,
+        Request $request,
+        CategoryRepository $categoryRepository
+    ): Response {
+
         $products = $productRepository->findAll();
 
         $form = $this->createForm(SearchProductType::class);
@@ -31,9 +39,40 @@ class ProductController extends AbstractController
             $products = $productRepository->findByName($data['search']);
         }
 
+        $categories = $categoryRepository->selectAllCategories();
+
         return $this->render('products/index.html.twig', [
             'products' => $products,
             'form' => $form->createView(),
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/nos-produits/{categoryName}", name="products_category")
+     * @param ProductRepository $productRepository
+     * @param Request $request
+     * @return Response
+     */
+    public function showByCategory(
+        $categoryName,
+        ProductRepository $productRepository,
+        Request $request
+    ): Response {
+
+        $products = $productRepository->findAll();
+
+        $productsByCategory = [];
+        foreach ($products as $product) {
+            if (substr($product->getCategory()[1], 0, strlen($categoryName)) == $categoryName) {
+                $productsByCategory[] = $product;
+            }
+        }
+
+        dump($productsByCategory);
+
+        return $this->render('products/show_products.html.twig', [
+            'products' => $productsByCategory,
         ]);
     }
 }
