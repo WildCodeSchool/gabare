b50e7dfb3416ac1c6c9e93b7ee8986e8610e5f25
Lionel Delamare <lionel.delamare@hotmail.com>
2020-01-15 14:25:40 +0100

    Delete comment

--- src/Form/ArticleTypeSearchType.php
+++ src/Form/ArticleTypeSearchType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\Theme;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -28,7 +27,6 @@ class ArticleTypeSearchType extends AbstractType
     {
         $resolver->setDefaults([
             'csrf_protection' => false,
-            // Configure your form options here
         ]);
     }
 }
